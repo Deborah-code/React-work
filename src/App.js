5044b30c7862f3abcb819876c2b570c359994b0b
Deborah-code <dbrhadewunmi@gmail.com>
2020-11-30 16:18:00 +0100

    added search feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import { render } from 'react-dom';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component'
+import Signup from "./components/Signup"
 
-function App() {
-  return (
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+  /* we're displaying the array(list) as elements on the webpage by creating a state and mapping it
+  the fetch function gets a list(array) from the api of the link passed to it, the .then creates a response
+  function that stores the data in a readable json file. the second .then now changes the state of the monsters
+  array already created (empty) withe the this.setstate function
+  */
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+      type = 'search'
+      placeholder = 'search monsters'
+      onChange={e => this.setState({ searchField: e.target.value })} 
+    />
+    <CardList monsters = {filteredMonsters} />
+
+
+      
     </div>
-  );
+    );
+
+
+    
+  }
 }
+
+
 
 export default App;
